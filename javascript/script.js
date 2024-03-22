6512cf2e5e9fc79229f124c717d7fa813533d842
jriveradepaz <jonathanrivera011@icloud.com>
2024-03-21 22:47:39 -0500

    css fixes
    attempted to make my css code better and more appropriate

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var submitButton = document.getElementById("click", function() {
+	alert("Thank you for subscribing!");
+  });
+});
--- script.js
+++
@@ -1,7 +0,0 @@
-function subscribed(){
-  document.getElementById('subscribeForm').addEventListener('submit', function(event){
-	  event.preventDefault();
-		document.getElementById('subscribeForm').style.display = 'none';
-		document.getElementById('thankYou').style.display = 'block';
-  });
-}
